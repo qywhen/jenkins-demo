57a0df1fff096dd1f2b8aa48e9d528a1efecb38d
whenqy <13121600523@163.com>
2019-11-26 15:55:34 +0800

    Initial commit

---
+++ src/main/java/com/wen/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,20 @@
+package com.wen.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+
+    @GetMapping("/jenkins")
+    public String jenkins() {
+        return "hello jenkins!!!11111";
+    }
+}
