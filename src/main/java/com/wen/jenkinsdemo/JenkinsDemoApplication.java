d56716df68a4637f9f91ef7c31509a9379d32e7b
whenqy <13121600523@163.com>
2019-11-26 16:00:33 +0800

    ff sdfadadsfsa:wq

--- src/main/java/com/wen/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/wen/jenkinsdemo/JenkinsDemoApplication.java
@@ -15,6 +15,6 @@ public class JenkinsDemoApplication {
 
     @GetMapping("/jenkins")
     public String jenkins() {
-        return "hello jenkins!!!11111";
+        return "hello jenkins!!!11122211";
     }
 }
