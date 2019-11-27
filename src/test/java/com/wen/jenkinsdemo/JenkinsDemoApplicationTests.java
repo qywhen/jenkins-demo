dc5855be242933913d7256729d16f5fd36e9a811
whenqy <13121600523@163.com>
2019-11-27 20:05:31 +0800

    111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223111223

--- src/test/java/com/wen/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/wen/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.wen.jenkinsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class JenkinsDemoApplicationTests {
 
     @Test
